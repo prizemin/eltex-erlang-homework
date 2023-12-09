6937ab4c90bef7fa9bf844832d9f99f7187f551a
prizemin <78478873+prizemin@users.noreply.github.com>
2023-12-10 04:26:18 +0700

    Fixed the error code style & process completion handling

--- hw-11/keylist_srv.erl
+++ hw-11/keylist_srv.erl
@@ -24,7 +24,7 @@
 -export([handle_info/2]).
 -export([terminate/2]).
 
--record(stateChild,{
+-record(state_child,{
   list = []    :: [{atom(), integer(), string()}],
   counter = 0  :: non_neg_integer()
 }).
@@ -67,59 +67,58 @@ stop(Name) ->
 
 %% Callbacks
 init([]) ->
-  process_flag(trap_exit, true),
   io:format("Process Init: ~n"),
-  {ok, #stateChild{}}.
+  {ok, #state_child{}}.
 
 handle_call({add, Key, Value, Comment},
     _From,
-    #stateChild{list = List, counter = Counter} = State) ->
+    #state_child{list = List, counter = Counter} = State) ->
   io:format("Added new element: ~p~n", [{Key, Value, Comment}]),
 %%  timer:sleep(10000),
 %%  1/0,
-  NewState = State#stateChild{list = List ++ [{Key, Value, Comment}], counter = Counter + 1},
-  {reply, {ok, NewState#stateChild.counter},  NewState};
+  NewState = State#state_child{list = List ++ [{Key, Value, Comment}], counter = Counter + 1},
+  {reply, {ok, NewState#state_child.counter},  NewState};
 
 handle_call({delete, Key},
     _From,
-    #stateChild{list = List, counter = Counter} = State) ->
+    #state_child{list = List, counter = Counter} = State) ->
   Element = lists:keyfind(Key, 1, List),
   io:format("Remoted element ~p with a key: ~p ~n", [Element, Key]),
   NewList = proplists:delete(Key, List),
-  NewState = State#stateChild{list = NewList, counter = Counter + 1},
-  {reply, {ok, NewState#stateChild.counter}, NewState};
+  NewState = State#state_child{list = NewList, counter = Counter + 1},
+  {reply, {ok, NewState#state_child.counter}, NewState};
 
 handle_call({is_member, Key},
     _From,
-    #stateChild{list = List, counter = Counter} = State) ->
+    #state_child{list = List, counter = Counter} = State) ->
   IsMember =  lists:keymember(Key, 1, List),
   io:format("Element with the key: ~p exist: ~p~n", [Key, IsMember]),
-  NewState = State#stateChild{counter = Counter + 1},
-  {reply, {IsMember, NewState#stateChild.counter}, NewState};
+  NewState = State#state_child{counter = Counter + 1},
+  {reply, {IsMember, NewState#state_child.counter}, NewState};
 
 handle_call({take, Key},
     _From,
-    #stateChild{list = List, counter = Counter} = State) ->
+    #state_child{list = List, counter = Counter} = State) ->
   case lists:keytake(Key, 1, List) of
     false ->
       {reply, {no_find}, State};
     {value, Element, NewList} ->
       io:format("New List: ~p~n", [NewList]),
       io:format("Element ~p with the key: ~p removed ~n", [Element, Key]),
-      NewState = State#stateChild{list = NewList, counter = Counter + 1},
-      {reply, {ok, Element, NewState#stateChild.counter}, NewState}
+      NewState = State#state_child{list = NewList, counter = Counter + 1},
+      {reply, {ok, Element, NewState#state_child.counter}, NewState}
   end;
 
 handle_call({find, Key},
     _From,
-    #stateChild{list = List, counter = Counter} = State) ->
+    #state_child{list = List, counter = Counter} = State) ->
   case lists:keyfind(Key, 1, List) of
     false ->
       {reply, no_find, State};
     {Key, _Value, _Comment} = Element ->
       io:format("Found element: ~p ~n", [Element]),
-      NewState = State#stateChild{counter = Counter + 1},
-      {reply, {ok, Element, NewState#stateChild.counter}, NewState}
+      NewState = State#state_child{counter = Counter + 1},
+      {reply, {ok, Element, NewState#state_child.counter}, NewState}
   end.
 
 handle_info({added_new_child, NewPid, NewName}, State) ->
