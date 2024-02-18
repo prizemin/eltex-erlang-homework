0e0d686e626d4005252acbf3087e354db2a148e1
prizemin <78478873+prizemin@users.noreply.github.com>
2024-02-18 07:32:01 +0700

    Initialize & configure project

---
+++ course-project/webRtp/README.md
@@ -0,0 +1,9 @@
+webRtp
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ course-project/webRtp/apps/webRtp/src/webRtp_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc webRtp public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(webRtp_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    webRtp_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
